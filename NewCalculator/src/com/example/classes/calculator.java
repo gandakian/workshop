46d69b1b6300f435394ab763a1bab5a84cadf79c
almighyt-amir <almighyt-amir@localhost>
2013-10-05 13:13:40 +0545

    My First Commit

---
+++ NewCalculator/src/com/example/classes/calculator.java
@@ -0,0 +1,16 @@
+package com.example.classes;
+
+public class calculator {
+	public int add(int i, int j){
+		return (i+j);
+	}
+	
+		public int mul(int i, int j){
+			return (i*j);
+		}
+		
+			public int div(int i, int j){
+				return (i/j);
+			}
+
+}
